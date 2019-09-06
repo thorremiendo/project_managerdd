b7f475dfe2b420d5effbf57bf7509d00486fd4af
ite19-3c-11@M303.uc-bcf.edu.ph <ite19-3c-11@M303.uc-bcf.edu.ph>
2019-09-07 07:58:27 +0800

    model

--- database/migrations/2019_09_06_224503_create_teams_table.php
+++ database/migrations/2019_09_06_224503_create_teams_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProjectsTable extends Migration
+class CreateTeamsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,8 @@ class CreateProjectsTable extends Migration
      */
     public function up()
     {
-        Schema::create('projects', function (Blueprint $table) {
+        Schema::create('teams', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('title');
-            $table->text('description')-> nullable();
-            $table->date('dueDate');
             $table->timestamps();
         });
     }
@@ -29,6 +26,6 @@ class CreateProjectsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('projects');
+        Schema::dropIfExists('teams');
     }
 }
---
+++ resources/views/projects/index.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.master')
+@section('content')
+	<div class="container">
+		<table class="table">
+		  <thead>
+		    <tr>
+		      <th scope="col">ID</th>
+		      <th scope="col">Title</th>
+		      <th scope="col">Description</th>
+		      <th scope="col">Due Date</th>
+		    </tr>
+		  </thead>
+		  <tbody>
+		    @foreach($projects as $project)
+		    <tr>
+		    	<td>{{ $project->id}} </td>
+		    	<td>{{ $project->title}} </td>
+		    	<td>{{ $project->description}} </td>
+		    	<td>{{ \Carbon\Carbon::parse($project->due_date)->toFormattedDateString() }} </td>
+		    </tr>
+		    @endforeach
+		  </tbody>
+		</table>
+	</div>
+@endsection
